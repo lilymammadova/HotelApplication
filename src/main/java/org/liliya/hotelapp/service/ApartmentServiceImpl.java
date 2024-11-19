fa313818aa8b316dca92582b65f106a50b584aab
lily <liliyamamedova391@gmail.com>
2024-11-20 01:56:05 +0400

    divided methods

--- src/main/java/org/liliya/hotelapp/service/ApartmentServiceImpl.java
+++ src/main/java/org/liliya/hotelapp/service/ApartmentServiceImpl.java
@@ -22,12 +22,26 @@ public class ApartmentServiceImpl implements ApartmentService {
         this.databaseConnection = databaseConnection;
     }
 
+    private static final String INSERT_APARTMENT_QUERY = "INSERT INTO apartments (price, status) VALUES (?, ?)";
+    private static final String INSERT_CLIENT_QUERY = "INSERT INTO clients (name) VALUES (?)";
+    private static final String FIND_AVAILABLE_APARTMENT_QUERY = "SELECT id FROM apartments WHERE status = ? LIMIT 1";
+    private static final String UPDATE_APARTMENT_AFTER_RESERVE_QUERY = "UPDATE apartments SET status = ?, client_id = ? WHERE id = ?";
+    private static final String FIND_CLIENT_QUERY = "SELECT client_id FROM apartments WHERE id = ?";
+    private static final String DELETE_CLIENT_QUERY = "DELETE FROM clients WHERE id = ?";
+    private static final String UPDATE_APARTMENT_AFTER_RELEASE_QUERY = "UPDATE apartments SET status = ?, client_id = NULL WHERE id = ?";
+    private static final String GET_PAGINATED_SORTED_APARTMENTS_QUERY = """
+                SELECT a.id, a.price, a.status, c.id AS client_id, c.name AS client_name
+                FROM apartments a
+                LEFT JOIN clients c ON a.client_id = c.id
+                ORDER BY %s
+                LIMIT ? OFFSET ?
+            """;
+
     @Override
     public int register(double price) {
-        String query = "INSERT INTO apartments (price, status) VALUES (?, ?)";
         int generatedId = -1;
         try (Connection connection = databaseConnection.getConnection();
-             PreparedStatement statement = connection.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS)
+             PreparedStatement statement = connection.prepareStatement(INSERT_APARTMENT_QUERY, PreparedStatement.RETURN_GENERATED_KEYS)
         ) {
             statement.setDouble(1, price);
             statement.setString(2, ReservationStatus.AVAILABLE.name());
@@ -53,52 +67,25 @@ public class ApartmentServiceImpl implements ApartmentService {
         if (!configuration.statusChangeAvailability()) {
             return false;
         }
-        String insertClientQuery = "INSERT INTO clients (name) VALUES (?)";
-        String findAvailableQuery = "SELECT id FROM apartments WHERE status = ? LIMIT 1";
-        String updateApartmentQuery = "UPDATE apartments SET status = ?, client_id = ? WHERE id = ?";
-
-
         try (Connection connection = databaseConnection.getConnection()) {
             connection.setAutoCommit(false);
 
-            int clientId = -1;
-            try (PreparedStatement insertClientStatement = connection.prepareStatement(insertClientQuery, PreparedStatement.RETURN_GENERATED_KEYS)) {
-                insertClientStatement.setString(1, client.getName());
-                insertClientStatement.executeUpdate();
-
-                try (ResultSet generatedKeys = insertClientStatement.getGeneratedKeys()) {
-                    if (generatedKeys.next()) {
-                        clientId = generatedKeys.getInt(1);
-                    }
-                }
+            int clientId = insertClient(connection, client);
+            if (clientId == -1) {
+                connection.rollback();
+                return false;
             }
-
-            int apartmentId = -1;
-            try (PreparedStatement findStatement = connection.prepareStatement(findAvailableQuery)) {
-                findStatement.setString(1, ReservationStatus.AVAILABLE.name());
-                try (ResultSet resultSet = findStatement.executeQuery()) {
-                    if (resultSet.next()) {
-                        apartmentId = resultSet.getInt("id");
-                    }
-                }
-            }
-
+            int apartmentId = findAvailableApartment(connection);
             if (apartmentId == -1) {
                 connection.rollback();
                 return false;
             }
-
-            try (PreparedStatement updateStatement = connection.prepareStatement(updateApartmentQuery)) {
-                updateStatement.setString(1, ReservationStatus.RESERVED.name());
-                updateStatement.setInt(2, clientId);
-                updateStatement.setInt(3, apartmentId);
-                if (updateStatement.executeUpdate() > 0) {
-                    connection.commit();
-                    return true;
-                }
+            if (updateApartmentAfterReserve(connection, apartmentId, clientId)) {
+                connection.commit();
+                return true;
+            } else {
+                connection.rollback();
             }
-
-            connection.rollback();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -110,67 +97,31 @@ public class ApartmentServiceImpl implements ApartmentService {
         if (!configuration.statusChangeAvailability()) {
             return false;
         }
-
-        String findClientQuery = "SELECT client_id FROM apartments WHERE id = ?";
-        String deleteClientQuery = "DELETE FROM clients WHERE id = ?";
-        String updateApartmentQuery = "UPDATE apartments SET status = ?, client_id = NULL WHERE id = ?";
-
         try (Connection connection = databaseConnection.getConnection()) {
             connection.setAutoCommit(false);
-
-            Integer clientId = null;
-
-            try (PreparedStatement findClientStatement = connection.prepareStatement(findClientQuery)) {
-                findClientStatement.setInt(1, id);
-                try (ResultSet resultSet = findClientStatement.executeQuery()) {
-                    if (resultSet.next()) {
-                        clientId = resultSet.getInt("client_id");
-                    }
-                }
-            }
-
+            Integer clientId = findClientByApartmentId(connection, id);
             if (clientId != null) {
-
-                try (PreparedStatement deleteClientStatement = connection.prepareStatement(deleteClientQuery)) {
-                    deleteClientStatement.setInt(1, clientId);
-                    deleteClientStatement.executeUpdate();
-                }
+                deleteClient(connection, clientId);
             }
-
-            try (PreparedStatement updateApartmentStatement = connection.prepareStatement(updateApartmentQuery)) {
-                updateApartmentStatement.setString(1, ReservationStatus.AVAILABLE.name());
-                updateApartmentStatement.setInt(2, id);
-                updateApartmentStatement.executeUpdate();
+            if (updateApartmentAfterRelease(connection, id)) {
+                connection.commit();
+                return true;
+            } else {
+                connection.rollback();
             }
-
-            connection.commit();
-            return true;
-
         } catch (SQLException e) {
             e.printStackTrace();
-            try (Connection connection = databaseConnection.getConnection()) {
-                connection.rollback(); // Откатываем изменения в случае ошибки
-            } catch (SQLException rollbackException) {
-                rollbackException.printStackTrace();
-            }
         }
-
         return false;
     }
+
 
     @Override
     public List<Apartment> getPaginatedAndSortedApartments(int page, int size, String sortBy) {
         List<Apartment> apartments = new ArrayList<>();
-        String query = """
-                    SELECT a.id, a.price, a.status, c.id AS client_id, c.name AS client_name
-                    FROM apartments a
-                    LEFT JOIN clients c ON a.client_id = c.id
-                    ORDER BY %s
-                    LIMIT ? OFFSET ?
-                """.formatted(sortBy);
 
         try (Connection connection = databaseConnection.getConnection();
-             PreparedStatement statement = connection.prepareStatement(query)) {
+             PreparedStatement statement = connection.prepareStatement(GET_PAGINATED_SORTED_APARTMENTS_QUERY.formatted(sortBy))) {
 
             int offset = (page - 1) * size;
 
@@ -199,4 +150,70 @@ public class ApartmentServiceImpl implements ApartmentService {
 
         return apartments;
     }
+
+    private int insertClient(Connection connection, Client client) throws SQLException {
+        try (PreparedStatement insertClientStatement = connection.
+                prepareStatement(INSERT_CLIENT_QUERY, PreparedStatement.RETURN_GENERATED_KEYS)) {
+            insertClientStatement.setString(1, client.getName());
+            insertClientStatement.executeUpdate();
+
+            try (ResultSet generatedKeys = insertClientStatement.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    return generatedKeys.getInt(1);
+                }
+            }
+        }
+        return -1;
+    }
+
+
+    private int findAvailableApartment(Connection connection) throws SQLException {
+        try (PreparedStatement findStatement = connection.prepareStatement(FIND_AVAILABLE_APARTMENT_QUERY)) {
+            findStatement.setString(1, ReservationStatus.AVAILABLE.name());
+            try (ResultSet resultSet = findStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    return resultSet.getInt("id");
+                }
+            }
+        }
+        return -1;
+    }
+
+    private boolean updateApartmentAfterReserve(Connection connection, int apartmentId, int clientId) throws SQLException {
+        try (PreparedStatement updateStatement = connection.prepareStatement(UPDATE_APARTMENT_AFTER_RESERVE_QUERY)) {
+            updateStatement.setString(1, ReservationStatus.RESERVED.name());
+            updateStatement.setInt(2, clientId);
+            updateStatement.setInt(3, apartmentId);
+            return updateStatement.executeUpdate() > 0;
+        }
+    }
+
+    private boolean updateApartmentAfterRelease(Connection connection, int apartmentId) throws SQLException {
+        try (PreparedStatement updateApartmentStatement = connection.prepareStatement(UPDATE_APARTMENT_AFTER_RELEASE_QUERY)) {
+            updateApartmentStatement.setString(1, ReservationStatus.AVAILABLE.name());
+            updateApartmentStatement.setInt(2, apartmentId);
+            return updateApartmentStatement.executeUpdate() > 0;
+        }
+    }
+
+
+    private Integer findClientByApartmentId(Connection connection, int apartmentId) throws SQLException {
+        try (PreparedStatement findClientStatement = connection.prepareStatement(FIND_CLIENT_QUERY)) {
+            findClientStatement.setInt(1, apartmentId);
+            try (ResultSet resultSet = findClientStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    return resultSet.getInt("client_id");
+                }
+            }
+        }
+        return null;
+    }
+
+    private void deleteClient(Connection connection, int clientId) throws SQLException {
+        try (PreparedStatement deleteClientStatement = connection.prepareStatement(DELETE_CLIENT_QUERY)) {
+            deleteClientStatement.setInt(1, clientId);
+            deleteClientStatement.executeUpdate();
+        }
+    }
+
 }
